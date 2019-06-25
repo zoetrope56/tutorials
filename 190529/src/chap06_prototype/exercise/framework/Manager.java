12424192dec5befe4409647c8e05142141e72c74
zoetrope56 <chldpwl5620@gmail.com>
2019-06-25 16:15:40 +0900

    eclipse commit

--- 190529/src/chap06_prototype/exercise/framework/Manager.java
+++ 190529/src/chap06_prototype/exercise/framework/Manager.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Manager {
 	private HashMap showcase = new HashMap();
-    public void register(String name, Product proto) {
+	public void register(String name, Product proto) {
         showcase.put(name, proto);
     }    
     
