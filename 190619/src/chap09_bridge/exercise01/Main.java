490cf0e3a5fc512823ce78e67734d6cf2401849c
zoetrope56 <chldpwl5620@gmail.com>
2019-07-22 17:15:40 +0900

    chap09 Bridge Pattern Exercise1

--- 190619/src/chap09_bridge/exercise01/Main.java
+++ 190619/src/chap09_bridge/exercise01/Main.java
@@ -5,8 +5,8 @@ import chap09_bridge.StringDisplayImpl;
 public class Main {
 	public static void main(String[] args) {
 		// Exercise 01
-		RandomDisplay d = new RandomDisplay(new StringDisplayImpl("Hello, Zoetrope"));
-		d.randomDisplay(10);
+		RandomDisplay d4 = new RandomDisplay(new StringDisplayImpl("Hello, Zoetrope"));
+		d4.randomDisplay(10);
 
 	}
 
